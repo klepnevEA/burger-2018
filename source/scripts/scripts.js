1c6686abc7aa64446f32325c80a93dbd9b605251
Klepnev <klepnev-ea@yandex.ru>
2018-01-27 18:48:44 +0300

    коммит

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -3,5 +3,83 @@
 $(document).ready(function() {
 	$(".reviews__text").dotdotdot({
 
-   });
+	});
+
+	// карта 
+
+
+	ymaps.ready(function () {
+		var myMap = new ymaps.Map('map', {
+			center: [55.751574, 37.573856],
+			zoom: 9
+		}, {
+			searchControlProvider: 'yandex#search'
+		}),
+
+
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        	'<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        	),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        	hintContent: 'Мы тут',
+        	balloonContent: 'Мы тут'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/icons/map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+
+        myPlacemark2 = new ymaps.Placemark([55.661574, 37.573856], {
+        	hintContent: 'Мы тут',
+        	balloonContent: 'Мы тут'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/icons/map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+        myPlacemark3 = new ymaps.Placemark([55.761574, 37.673856], {
+        	hintContent: 'Мы тут',
+        	balloonContent: 'Мы тут'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/icons/map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+        myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemark2)
+        .add(myPlacemark3);
+
+        myMap.behaviors.disable('scrollZoom');
+    });
 });
